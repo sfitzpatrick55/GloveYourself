4379593bce3335e37e20b3b01d3feb556ffd67d8
Scott <sfitzpatrick55@gmail.com>
2022-07-24 14:59:48 -0400

    Glove controller, detail view, and index view

---
+++ GloveYourself.WebMVC/Controllers/GloveController.cs
@@ -0,0 +1,18 @@
+﻿using GloveYourself.Data.Models;
+using GloveYourself.Models.Glove;
+using GloveYourself.WebMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloveYourself_BlazorWASM.Server.Controllers;
+
+    public class GloveController : Controller
+    {
+        // GET: Glove
+        public ActionResult Index()
+        {
+        var model = new GloveListItem[0];
+            return View();
+        }
+    }
+
